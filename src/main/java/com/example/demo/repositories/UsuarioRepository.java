0df512e3283a608caa3886256209b233116c49e2
sam <samueldazadaza@gmail.com>
2022-09-29 11:54:37 -0500

    proyecto funcionando stock libros

---
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.UsuarioModel;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
+
+}
